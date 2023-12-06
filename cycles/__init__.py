3eee6ef65b92f0c782ea4d7d8e925bcecdcef8c2
feihoo87 <xfh1987@gmail.com>
2023-12-06 23:44:13 +0800

    add SU(n)

--- cycles/__init__.py
+++ cycles/__init__.py
@@ -3,3 +3,4 @@ from .cycles import Cycles, find_permutation, permute, random_permutation
 from .group import Coset, PermutationGroup
 from .named_group import (AbelianGroup, AlternatingGroup, CyclicGroup,
                           DihedralGroup, SymmetricGroup)
+from .special_unitary_group import SU
