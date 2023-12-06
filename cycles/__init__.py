e55caf21369f60abaa7e4e7afca36420a6c1e3b2
feihoo87 <xfh1987@gmail.com>
2023-12-06 22:12:23 +0800

    init commit

---
+++ cycles/__init__.py
@@ -0,0 +1,4 @@
+from .cycles import Cycles, find_permutation, permute, random_permutation
+from .group import Coset, PermutationGroup
+from .named_group import (AbelianGroup, AlternatingGroup, CyclicGroup,
+                          DihedralGroup, SymmetricGroup)
