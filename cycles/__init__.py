6f51d75a9ae97c6d845a4bce8d8dccb3593d159e
feihoo87 <xfh1987@gmail.com>
2023-12-06 22:47:27 +0800

    add clifford

--- cycles/__init__.py
+++ cycles/__init__.py
@@ -1,3 +1,4 @@
+from .clifford import CliffordGroup
 from .cycles import Cycles, find_permutation, permute, random_permutation
 from .group import Coset, PermutationGroup
 from .named_group import (AbelianGroup, AlternatingGroup, CyclicGroup,
