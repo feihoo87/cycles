2515089c2fb6deec41ee6ffb284bf30fcce827ce
feihoo87 <xfh1987@gmail.com>
2023-12-12 15:03:11 +0800

    remove circuits deps in clifford mod

--- cycles/clifford/__init__.py
+++ cycles/clifford/__init__.py
@@ -1,6 +1,3 @@
-from .funtions import (cliffordOrder, one_qubit_clifford_matricies,
-                       one_qubit_clifford_mul_table, one_qubit_clifford_seq,
-                       two_qubit_clifford_matricies, twoQubitCliffordSequence)
-from .group import CliffordGroup, find_permutation_for_Unitary
+from .group import CliffordGroup, cliffordOrder
 from .paulis import (decode_paulis, encode_paulis, imul_paulis, mul_paulis,
                      string_to_matrices)
